6ed41507eb3d7b5aa2614c10ef8df316d6d86f01
Pablo Schmeiser <upwhd@student.kit.edu>
2024-04-11 02:56:24 +0200

    Solution to 10_macros_calling_macros

--- exercises/10_macros_calling_macros/main.rs
+++ exercises/10_macros_calling_macros/main.rs
@@ -33,7 +33,11 @@ macro_rules! digit {
 
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `number!()` macro.
+macro_rules! number {
+    ($($n:ident )+) => {
+        (concat!($(digit!($n)),+))
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
